5993fccdeb1f06d4614920a93417681cdc1d856d
ticchen <ticchen@gmail.com>
2022-01-05 16:59:45 +0800

    support build v5.13.0 module

--- pl2303.c
+++ pl2303.c
@@ -1,5 +1,11 @@
 #include <linux/kernel.h>
 #include <linux/version.h>
 
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 13, 0)
+#include "v5.13.0/pl2303.c"
+
+#else
 #include "legacy/pl2303.c"
 
+#endif
